025cf543cc45847bb6fdcad84c38489cfe21f594
LUIS NUNEZ <lnkintero@gmail.com>
2021-08-29 23:29:44 -0400

    Update reserva

--- db/migrate/20210830020322_create_reservas.rb
+++ db/migrate/20210830020322_create_reservas.rb
@@ -1,7 +1,7 @@
 class CreateReservas < ActiveRecord::Migration[6.0]
   def change
     create_table :reservas do |t|
-      t.reference :users
+      t.references :user, null: false, foreign_key: true
       t.boolean :status
       t.string :problemas
       t.integer :pago_diagnosticos
