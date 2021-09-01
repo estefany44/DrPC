432b4a19e24d7f8582058e393a2ca5dfd4e35640
gonzaloaranda <gonzalo.aranda@hotmail.com>
2021-09-01 12:08:03 -0400

    actualiza apprecord

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatabl
+         :recoverable, :rememberable, :validatable
          
   has_many :appointments
   has_many :orders, through: :appointments
