4f78db343dccfe8075383dee0c3ca7ee6ca645da
estefany romo <estefanyromoalvarez@gmail.com>
2021-09-06 11:12:17 -0300

    changes appointments form and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,32 +4,32 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'services', to: 'pages#services'
 #begin orders ---------------------------------------------------
-  get 'orders', to: 'orders#index'
-  get 'orders/new', to: 'orders#new', as: :new
+#   get 'orders', to: 'orders#index'
+#   get 'orders/new', to: 'orders#new', as: :new
 
-  get 'orders/:id', to: 'orders#show', as: :order
+#   get 'orders/:id', to: 'orders#show', as: :order
 
-  post 'orders', to: 'orders#create'
+#   post 'orders', to: 'orders#create'
 
-  get 'orders/:id/edit', to: 'orders#edit', as: :edit_order
-  patch 'orders/:id', to: 'orders#update'
+#   get 'orders/:id/edit', to: 'orders#edit', as: :edit_order
+#   patch 'orders/:id', to: 'orders#update'
 
-  #delete
-  delete 'orders/:id', to: 'orders#destroy'
+#   #delete
+#   delete 'orders/:id', to: 'orders#destroy'
+  resources :orders
+# #end orders ----------------------------------------------------
+#   get 'appointments', to: 'appointments#index'
+#   get 'appointments/new', to: 'appointments#new', as: :new
 
-#end orders ----------------------------------------------------
-  get 'appointments', to: 'appointments#index'
-  get 'appointments/new', to: 'appointments#new', as: :new
+#   get 'appointments/:id', to: 'appointments#show', as: :appointment
 
-  get 'appointments/:id', to: 'appointments#show', as: :appointment
+#   post 'appointments', to: 'appointments#create'
 
-  post 'appointments', to: 'appointments#create'
+#   get 'appointments/:id/edit', to: 'appointments#edit', as: :edit_appointment
+#   patch 'appointments/:id', to: 'appointments#update'
 
-  get 'appointments/:id/edit', to: 'appointments#edit', as: :edit_appointment
-  patch 'appointments/:id', to: 'appointments#update'
-
-  #delete
-  delete 'appointments/:id', to: 'appointments#destroy'
+#   #delete
+#   delete 'appointments/:id', to: 'appointments#destroy'
 
   resources :appointments
 end
