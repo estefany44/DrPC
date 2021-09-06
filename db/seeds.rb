da1539ccae266e7687bba766420d245dbe4ca05a
titi noguchi <cristina.noguchi@gmail.com>
2021-09-06 09:42:16 -0300

    cambio del seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,10 @@ User.destroy_all
 puts "Creando Usuarios"
 User.create(first_name: "Aministrador", last_name: "DrPC", rut: "9999999-9", client: false, address: "Avda Plama 38", phone: "991474434", email: "admin@dominio.com", password: "password")
 puts "Creando Appoitnments"
-<<<<<<< HEAD
+
 Appointment.create(user: User.last, description: "Descripcion", status: "Estado de la reserva")
 puts "Terminado"
 
-
-=======
 Appointment.create(user: User.last, description: "No enciende", status: "Pendiente")
 puts "Creando (4) Orders"
 Order.create(appointment:Appointment.last, diagnostic:"Falla en Pantalla", solution:"Cambio de Pantalla", status:"Abierta", price:15000)
@@ -18,4 +16,3 @@ Order.create(appointment:Appointment.last, diagnostic:"Falla en Procesador", sol
 Order.create(appointment:Appointment.first, diagnostic:"Cabezal de impresora sucio", solution:"Limpieza de cabezal", status:"Abierta", price:5000)
 Order.create(appointment:Appointment.first, diagnostic:"Falla de Cargador", solution:"Soldadura de cableado interno", status:"Cerrada", price:10000)
 puts "Terminado"
->>>>>>> 4ef48a38878ecd5c835e5ba44846501107545cf4
