a75317e46fd53aa162fe13cfd066d0105d4071f1
gonzaloaranda <gonzalo.aranda@hotmail.com>
2021-09-21 01:56:46 -0300

    clean code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :services]
+  skip_before_action :authenticate_user!, only: [:home, :services, :test]
 
   def home
   end
 
   def services
+  end
+
+  def test
   end
 end
