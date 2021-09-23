1e4ec69727fe16962d369595540d009404d63bac
LUIS NUNEZ <lnkintero@gmail.com>
2021-09-23 01:33:48 -0300

    Servicios

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :services, :tutorials, :test, :description]
+  skip_before_action :authenticate_user!, only: [:home, :services, :tutorials, :description]
 
   def home
   end
